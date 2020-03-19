3813e271eb7e330b7716138b741c67f5e75c4d8d
a-hacker <explosivepyrounicorns@gmail.com>
2020-03-18 23:05:28 -0400

    Add status icon next to ticket names

--- app/components/TicketSidebar.js
+++ app/components/TicketSidebar.js
@@ -14,12 +14,37 @@ export default class TicketSidebar extends Component<Props> {
   render() {
     const { project, updateView } = this.props;
 
+    const jiraColorToCssColor = color => {
+      switch (color) {
+        case 'blue-gray':
+          return 'Blue';
+        case 'yellow':
+          return 'Gold';
+        case 'green':
+          return 'Green';
+        default:
+          return 'Azure';
+      }
+    };
+
     const tickets = Array.from(project.tickets.values(), ticket => (
       <button
         type="button"
         onClick={() => updateView({ currentTicket: ticket })}
       >
-        {ticket.key}
+        <div className={style.ticket}>
+          <svg height="10" width="10">
+            <circle
+              cx="5"
+              cy="4"
+              r="3"
+              stroke="white"
+              strokeWidth="0"
+              fill={jiraColorToCssColor(ticket.status.color)}
+            />
+          </svg>
+          <a>{ticket.key}</a>
+        </div>
       </button>
     ));
 
